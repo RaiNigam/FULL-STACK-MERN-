cfd96fb5f4d0f0fa389ae6a11624edc0e0c7d844
Zeroflash1 <nigamrai2020@gmail.com>
2023-03-04 23:05:52 +0545

    counter and event assignment added

---
+++ JS Assignments/JS Counter Assignment/MouseOver/script.js
@@ -0,0 +1,18 @@
+const counter=document.querySelector('.counter');
+const btn=document.querySelectorAll('.btn');
+console.log(btn);
+let count=0;
+btn.forEach((e)=>{
+    e.addEventListener('mouseover',()=>{
+        if(e.classList.contains('increase')){
+            count=count+1;
+            counter.innerText=count;
+        }else if(e.classList.contains('decrease')){
+            count=count-1;
+            counter.innerText=count;
+        }else{
+            count=0;
+            counter.innerText=count;
+        }
+    });
+});
