0eb276531e43177f569bdc6d305936e0cee4f76f
Zeroflash1 <nigamrai2020@gmail.com>
2023-06-14 05:28:38 +0545

    Destructuring assignments added

---
+++ Destructuring Assignment/5.Min and Max Values/script.js
@@ -0,0 +1,11 @@
+function findMinMax(arr){
+
+    console.log(...arr);
+    console.log(...arr);
+// const max=Math.max(...arr);
+// const min=Math.min(...arr);
+// const obj={Max:max,Min:min};
+// return obj;
+}
+let array=[5,2,7,1,9];
+console.log(findMinMax(array));
